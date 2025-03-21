eae558e9aa405a6f6d399e38cd2c3450d526bb2b
Wanderson Camargo <wandersonwhcr@gmail.com>
2025-03-21 08:08:51 -0300

    Define client

--- client/main.go
+++ client/main.go
@@ -1,9 +1,43 @@
 package client
 
 import (
+	"context"
 	"log"
+	"os"
+	"time"
+
+	"github.com/wandersonwhcr/grpc-hostname/proto"
+	"google.golang.org/grpc"
 )
 
 func Run() {
-	log.Printf("Client")
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		log.Fatalf("Failed to Get Hostname: %v", err)
+	}
+
+	conn, err := grpc.Dial(os.Getenv("HOSTNAME_SERVER_ADDR"), grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("Failed to Connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := proto.NewHostnameClient(conn)
+
+	ticker := time.NewTicker(time.Second * 2)
+
+	defer ticker.Stop()
+
+	for range ticker.C {
+		response, err := c.GetHostname(context.Background(), &proto.GetHostnameRequest{Hostname: hostname})
+
+		if err != nil {
+			log.Fatalf("Failed to Get Server Hostname: %v", err)
+		}
+
+		log.Printf("Client: %s, Server: %s", hostname, response.Hostname)
+	}
 }
