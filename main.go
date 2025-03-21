59265873564c39f4789180ed42b0a9697eba8cd6
Wanderson Camargo <wandersonwhcr@gmail.com>
2025-03-21 07:55:13 -0300

    Initialize client

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/wandersonwhcr/grpc-hostname/client"
 	"github.com/wandersonwhcr/grpc-hostname/server"
 )
 
 func main() {
-	server.Run()
+	switch os.Getenv("HOSTNAME_CMD") {
+		case "server":
+			server.Run()
+		case "client":
+			client.Run();
+		default:
+			log.Fatalf("Failed to Command: server or client")
+	}
 }
