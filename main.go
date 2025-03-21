8dffeec10bdac46a4884e7174326d68489c8515b
Wanderson Camargo <wandersonwhcr@gmail.com>
2025-03-21 07:46:07 -0300

    Isolate source

--- main.go
+++ main.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"log"
-	"net"
-	"os"
-
-	"github.com/wandersonwhcr/grpc-hostname/proto"
-	"google.golang.org/grpc"
+	"github.com/wandersonwhcr/grpc-hostname/server"
 )
 
-type server struct {
-	proto.UnimplementedHostnameServer
-}
-
 func main() {
-	addr := os.Getenv("HOSTNAME_SERVER_ADDR")
-
-	l, err := net.Listen("tcp", addr)
-
-	if err != nil {
-		log.Fatalf("Failed to Listen: %v", err)
-	}
-
-	s := grpc.NewServer()
-
-	proto.RegisterHostnameServer(s, &server{})
-
-	log.Printf("Listening: %s", addr)
-
-	if err := s.Serve(l); err != nil {
-		log.Fatalf("Failed to Serve: %v", err)
-	}
+	server.Run()
 }
---
+++ server/main.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"github.com/wandersonwhcr/grpc-hostname/proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	proto.UnimplementedHostnameServer
+}
+
+func Run() {
+	addr := os.Getenv("HOSTNAME_SERVER_ADDR")
+
+	l, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to Listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	proto.RegisterHostnameServer(s, &server{})
+
+	log.Printf("Listening: %s", addr)
+
+	if err := s.Serve(l); err != nil {
+		log.Fatalf("Failed to Serve: %v", err)
+	}
+}
